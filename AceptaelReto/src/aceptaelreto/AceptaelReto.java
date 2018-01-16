005a99305d7436756a2539b826fde91a4667986f
Javier <javierbonetp@gmail.com>
2018-01-16 17:41:08 +0100

    Acepta el Reto yo soy tu,

--- AceptaelReto/src/aceptaelreto/AceptaelReto.java
+++ AceptaelReto/src/aceptaelreto/AceptaelReto.java
@@ -16,26 +16,27 @@ public class AceptaelReto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner txt0=new Scanner(System.in);
-        Scanner txt1=new Scanner(System.in);
-        Scanner sc=new Scanner(System.in);
+        Scanner Scan=new Scanner(System.in);
         
         
         String texto0;
         String texto1;
-        int lin;
+        int casos;
         int i;
         
         //System.out.println("Dime cuantas lineas: ");
-        lin=sc.nextInt();
-        for(i=0;i<lin;i++){
+        casos=Scan.nextInt();
+        
+        for(i=0;i<casos;i++){
             //System.out.println("Dime: ");
-            texto0=txt0.nextLine();
+            texto0 = Scan.nextLine();
+            texto1 = Scan.nextLine();
+            
             //System.out.println("Dime dime: ");
-            texto1=txt1.nextLine();
-            if (texto0.equals("Luke") && texto1.equals("padre")){
-                System.out.println("TOP SECRET");
+            if(texto0.equals("Luke")&&texto1.equals("padre")){
+                    System.out.println("TOP SECRET");
             }else{
+                
                 System.out.println( texto0 + ", yo soy tu " + texto1);
             }
         }
