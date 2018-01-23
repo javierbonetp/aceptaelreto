28e77715c3ed07248ecf59219e2e2f720982e62e
Javier <javierbonetp@gmail.com>
2018-01-23 18:43:48 +0100

    Acepta el Reto Fuerzas Imperiales

--- AceptaelReto/src/aceptaelreto/FuerzasImperiales.java
+++ AceptaelReto/src/aceptaelreto/FuerzasImperiales.java
@@ -11,6 +11,53 @@ package aceptaelreto;
  */
 import java.util.Scanner;
 public class FuerzasImperiales {
+    static boolean Milenario(char[][] celda){
+        int Si=0;
+        int Sj=0;
+        int Fi;
+        int Fj;
+        
+        for(int i=0;i<celda.length;i++){
+            for(int j=0;j<celda[i].length;j++){
+                if(celda[i][j]=='S'){
+                    Si=i;
+                    Sj=j;
+                }
+                if(celda[i][j]=='F'){
+                    Fi=i;
+                    Fj=j;
+                }
+            }
+        }
+        for(int i=Si;i<celda.length;i++){
+            for(int j=Sj;j<celda[i].length;j++){
+                if((j+1)<celda[i].length){
+                    if((celda[i][j+1]=='.') && ((j+1)<=celda[0].length)){
+                    Milenario(celda);
+                    }
+                    if(celda[i][j+1]=='*'){
+                    return false;
+                    }
+                    if(celda[i][j+1]=='F'){
+                    return true;
+                    }
+                }
+                if(((i+1)<celda.length) && ((i+1)<=celda.length)){
+                    if(celda[i+1][j]=='.'){
+                    Milenario(celda);
+                    }
+                    if(celda[i+1][j]=='*'){
+                    return false;
+                    }
+                    if(celda[i+1][j]=='F'){
+                    return true;
+                    }
+                }
+            }
+        }
+        //?¿
+        return true;
+    }
     static void MostrarMapa(char[][] celda){
         System.out.print("Mapa:\n");
         for(int i=0;i<celda.length;i++){
@@ -40,10 +87,14 @@ public class FuerzasImperiales {
                 celda[i][j]=temporal.charAt(j);
             }
         }
-        
-        MostrarMapa(celda);
-        
-        
+        //Muestra el mapa
+        //MostrarMapa(celda);
+        if(Milenario(celda)==true){
+            Milenario(celda);
+            System.out.println("SI");
+        }else{
+            System.out.println("NO");
+        }
         
     }
     
