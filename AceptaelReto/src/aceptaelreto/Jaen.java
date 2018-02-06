52b562d6d7d2b8118451cdac1c69850ada61c5e8
Javier <javierbonetp@gmail.com>
2018-02-06 16:42:23 +0100

    Acepta el Reto Jaen definitivo

--- AceptaelReto/src/aceptaelreto/Jaen.java
+++ AceptaelReto/src/aceptaelreto/Jaen.java
@@ -3,43 +3,27 @@ package aceptaelreto;
 
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author lioalc
- */
 public class Jaen {
 
-    // FUNCION RECURSIVA: Devuelve tamaño de la plantacion en (x,y)
     private static int tamPlantacion(char mapa[][], int x, int y) {
         
-        // Creamos variable para tamaño
         int tam = 0;
         
-        // Comprobamos si esta fuera de rango
         if (x < 0 || y < 0 || x >= mapa.length || y >= mapa[0].length) {
             return 0;
         }
 
-        // Si espacio vacio, devolvemos tamaño 0
         if (mapa[x][y] == ' ') {
             return 0;
         }
 
-        // Si cultivable, tamaño 1 + comprobamos zonas adyacentes
         if (mapa[x][y] == '#') {
             
-            // Eliminamos la zona cultivable (x,y) para evitar bucle infinito
+            // Evitar bucle infinito
             mapa[x][y] = ' ';
             
-            // Añadimos 1 (zona cultivable actual)
             tam++;
 
-            // Sumamos tamaño plantacion zonas adyacentes
             tam += tamPlantacion(mapa, x + 1, y);
             tam += tamPlantacion(mapa, x - 1, y);
             tam += tamPlantacion(mapa, x, y + 1);
@@ -49,7 +33,6 @@ public class Jaen {
             return tam;
         }
 
-        // Por si el codigo llega aqui (no deberia), devolvemos cero
         return 0;
 
     }
